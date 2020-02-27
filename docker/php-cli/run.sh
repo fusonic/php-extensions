98f03f13fa0d98f46cc72f5de8d36bb4eb8bee62
Arjan Frans <arjan.frans@fusonic.net>
2020-02-27 14:10:44 +0100

    Add xdebug to build image

--- docker/php-cli/run.sh
+++ docker/php-cli/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -e
 
 DIRECTORY="${1:-packages}"
+IP_ADDRESS=`ip -o route get to 8.8.8.8 | sed -n 's/.*src \([0-9.]\+\).*/\1/p'`
 
 docker build -t extensions-php-cli docker/php-cli
-docker run -it -v ${PWD}/${DIRECTORY}:/app --workdir /app --entrypoint /bin/bash extensions-php-cli
+docker run -it -v ${PWD}:/app \
+  --workdir /app/${DIRECTORY} \
+  -e "XDEBUG_CONFIG=\"remote_host=${IP_ADDRESS}\"" \
+  --entrypoint /bin/bash extensions-php-cli
