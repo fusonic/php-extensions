dd614c68c5bdb721a8c78b760127bdfb5a7cddb1
Dominic Luidold <dominic.luidold@fusonic.net>
2023-10-13 06:53:45 +0000

    cs(#57): Unify header license comment

--- packages/http-kernel-bundle/.php-cs-fixer.dist.php
+++ packages/http-kernel-bundle/.php-cs-fixer.dist.php
@@ -2,14 +2,20 @@
 
 declare(strict_types=1);
 
-$finder = (new PhpCsFixer\Finder())
+$finder = PhpCsFixer\Finder::create()
     ->in(__DIR__);
 
 return (new PhpCsFixer\Config())
     ->setRules([
+        '@PHP82Migration' => true,
         '@Symfony' => true,
         '@Symfony:risky' => true,
         'declare_strict_types' => true,
+        'header_comment' => [
+            'header' => 'Copyright (c) Fusonic GmbH. All rights reserved.'.\PHP_EOL.'Licensed under the MIT License. '.
+                'See LICENSE file in the project root for license information.',
+            'location' => 'after_open',
+        ],
         'no_useless_else' => true,
         'no_useless_return' => true,
         'nullable_type_declaration_for_default_null_value' => true,
